24d4dac8fa9abc068c057ff6c5d6663e77052824
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-02-02 23:43:24 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -2,77 +2,51 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 function createBoxes(amount) {
-    console.log('amount', amount);
+  // console.log('amount', amount);
 
-  console.log('cr');
-  makeImage(amount);
-  // return image;
-}
+  const boxes = document.querySelector('#boxes');
+  let divSize = 20;
+    
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    
+    divSize += 10;
+    box.style.width = `${divSize}px`;
+    box.style.height = `${divSize}px`;
+    // console.log(box.style.width);
+    // console.log(box.style.height);
+    // box.textContent = 'divan';
 
-function makeImage(amount) {
-  // const image1 = document.createElement("img");
-  
-
-
-  const makeImageMarkup = image => {
-  return `
-  <div></div> 
-  `;
+    boxes.append(box);
   }
-const imageEl = document.querySelector('div#boxes');
-  const makeImages = makeImageMarkup;
-  imageEl.insertAdjacentHTML('beforeend', makeImageMarkup);
-  
-
-console.log(makeImages);
-
-//   for (let i = 0; i < amount; i += 1) {
-//     image.src = "https://placeimg.com/640/480/nature";
-//     image.alt = "Nature";
-//     console.log(image);
-
-//   }
-//     console.log('amount', amount);
-
-//     console.log(image);
-
-// // imageEl.insertAdjacentHTML('beforeend', makeImages);
-
-//   return image;
 }
-
-
-
-
 
 function destroyBoxes() {
-  console.log('de');
-
-
+  boxes.remove();
+  const controls = document.querySelector('#controls');
+  const box = document.createElement('div');
+  box.id = 'boxes';
+  controls.after(box);
 }
 
 
 const createButton = document.querySelector('button[data-create');
-// console.log(createButton);
 const destroyButton = document.querySelector('button[data-destroy]');
-// console.log(destroyButton);
 
-function aaa() {
+function checkAmount() {
   const amount = document.querySelector('input').value;
-  // console.log(amount);
   
   if (amount > 0 && amount < 101) {
-    console.log(amount);
+    // console.log(amount);
     createBoxes(amount);
 
   } else {
-  // alert("Enter value from 1 to 100!");
+    alert("Enter value from 1 to 100!");
   }
 }
 
-
-createButton.addEventListener("click", aaa);
+createButton.addEventListener("click", checkAmount);
 destroyButton.addEventListener("click", destroyBoxes);
 
